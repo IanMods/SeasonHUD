75ed00d947fb6d2cd596087690b99d9be8ff9563
Ian Anderson <iananderson92@gmail.com>
2022-10-20 21:58:21 -0700

    Serine Seasons API working. Need to get season detection working

--- src/main/java/club/iananderson/seasoninfo/client/SeasonHudOverlay.java
+++ src/main/java/club/iananderson/seasoninfo/client/SeasonHudOverlay.java
@@ -1,12 +1,27 @@
 package club.iananderson.seasoninfo.client;
 
 import club.iananderson.seasoninfo.Seasoninfo;
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import sereneseasons.api.season.ISeasonState;
+import sereneseasons.api.season.Season;
+import sereneseasons.api.season.SeasonHelper;
+import sereneseasons.config.BiomeConfig;
+import sereneseasons.api.season.Season.SubSeason;
+import sereneseasons.config.ServerConfig;
+import sereneseasons.season.SeasonHooks;
+
+import java.util.Objects;
 
 public class SeasonHudOverlay {
 
@@ -19,11 +34,12 @@ public class SeasonHudOverlay {
     private static final ResourceLocation WINTER = new ResourceLocation(Seasoninfo.MODID,
             "textures/season/winter.png");
 
-    public static final IGuiOverlay HUD_SEASON = ((gui, poseStack, partialTick,screenWidth, screenHeight) -> {
+    public static final IGuiOverlay HUD_SEASON = (gui, poseStack, partialTick,screenWidth, screenHeight) -> {
         int x = 0;
         int y = 0;
         int iconDim = 12;
         int offsetDim = 5;
+
 
         String currentSeason = "Summer";
         gui.getFont().draw(poseStack,currentSeason, (float) (x+20), (float) (y+offsetDim+(.25*iconDim)),0xffffffff);
@@ -33,5 +49,5 @@ public class SeasonHudOverlay {
         RenderSystem.setShaderTexture(0,SUMMER);
         GuiComponent.blit(poseStack,x+offsetDim, y+offsetDim,0,0,iconDim,iconDim,iconDim,iconDim);
 
-    });
+    };
 }
