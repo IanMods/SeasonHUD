be4b0b72009547f58e5ab528af3ba3a12783409a
Ian Anderson <iananderson92@gmail.com>
2022-10-22 18:39:28 -0700

    Season icon is working. Next is config file

--- src/main/java/club/iananderson/seasoninfo/client/SeasonHudOverlay.java
+++ src/main/java/club/iananderson/seasoninfo/client/SeasonHudOverlay.java
@@ -4,55 +4,45 @@ import club.iananderson.seasoninfo.Seasoninfo;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import sereneseasons.api.season.Season;
 import sereneseasons.api.season.SeasonHelper;
 
+import javax.annotation.Nullable;
+
 public class SeasonHudOverlay {
-
-//    public Season currentSeason = SeasonHelper.getSeasonState(Minecraft.getInstance().level).getSeason();
-//    public class SeasonName{
-//        private String SEASONNAMEOLD;
-//        public String getName(){
-//            return SEASONNAMEOLD;
-//        }
-//        public void setName(String SEASONNAME) {
-//            this.SEASONNAMEOLD = SEASONNAME;
-//       }
-//    }
-
-
-    private static final ResourceLocation SPRING = new ResourceLocation(Seasoninfo.MODID,
-            "textures/season/spring.png");
-    private static final ResourceLocation SUMMER = new ResourceLocation(Seasoninfo.MODID,
-            "textures/season/summer.png");
-    private static final ResourceLocation FALL = new ResourceLocation(Seasoninfo.MODID,
-            "textures/season/fall.png");
-    private static final ResourceLocation WINTER = new ResourceLocation(Seasoninfo.MODID,
-            "textures/season/winter.png");
-    //private static final ResourceLocation SEASONNAME = new ResourceLocation(Seasoninfo.MODID,
-    //        "textures/season/"+ SEASON + ".png");
+    @Nullable
+    public static Season SeasonVal(){
+        ClientLevel MCLEVEL = Minecraft.getInstance().level;
+        Season seasonValue = SeasonHelper.getSeasonState(MCLEVEL).getSeason();
+        return seasonValue;
+    }
 
     public static final IGuiOverlay HUD_SEASON = (gui, poseStack, partialTick,screenWidth, screenHeight) -> {
         int x = 0;
         int y = 0;
-        int iconDim = 12;
+        int iconDim = 16;
         int offsetDim = 5;
 
-        Season currentSeason = SeasonHelper.getSeasonState(Minecraft.getInstance().level).getSeason();
+        ResourceLocation SEASON = new ResourceLocation(Seasoninfo.MODID,
+            "textures/season/"+SeasonVal().name().toLowerCase()+".png");
+
+        ClientLevel MCLEVEL = Minecraft.getInstance().level;
+
+        Season currentSeason = SeasonHelper.getSeasonState(MCLEVEL).getSeason();
         String SEASONNAME = currentSeason.name();
         String seasonLower = SEASONNAME.toLowerCase();
         String seasonCap = seasonLower.substring(0,1).toUpperCase()+ seasonLower.substring(1);
 
         //String currentSeason = "Summer";
-        gui.getFont().draw(poseStack,seasonCap, (float) (x+20), (float) (y+offsetDim+(.25*iconDim)),0xffffffff);
+        gui.getFont().draw(poseStack,seasonCap, (float) (x+25), (float) (y+offsetDim+(.25*iconDim)),0xffffffff);
 
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
-        RenderSystem.setShaderTexture(0,WINTER);
+        RenderSystem.setShaderTexture(0,SEASON);
         GuiComponent.blit(poseStack,x+offsetDim, y+offsetDim,0,0,iconDim,iconDim,iconDim,iconDim);
-
     };
 }
