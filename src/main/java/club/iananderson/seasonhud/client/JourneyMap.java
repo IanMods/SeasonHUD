b903515c6ed9594e30349b8dfa3856f97738032c
Ian Anderson <iananderson92@gmail.com>
2022-11-17 06:15:20 -0700

    Continued JourneyMap integration.
    Position is correct. Need to figure out the colors now

--- src/main/java/club/iananderson/seasonhud/client/JourneyMap.java
+++ src/main/java/club/iananderson/seasonhud/client/JourneyMap.java
@@ -5,6 +5,9 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import journeymap.client.JourneymapClient;
 import journeymap.client.api.display.Context;
 import journeymap.client.api.model.TextProperties;
+import journeymap.client.cartography.color.ColorManager;
+import journeymap.client.cartography.color.ColorPalette;
+import journeymap.client.io.ThemeLoader;
 import journeymap.client.properties.InGameMapProperties;
 import journeymap.client.properties.MiniMapProperties;
 import journeymap.client.render.draw.DrawUtil;
@@ -14,15 +17,18 @@ import journeymap.client.ui.minimap.DisplayVars;
 import journeymap.client.ui.theme.Theme;
 import journeymap.client.ui.theme.Theme.Minimap.MinimapSpec;
 import journeymap.client.ui.theme.ThemeLabelSource;
+import journeymap.client.ui.theme.impl.FlatTheme;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import net.minecraftforge.fml.ModList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -47,12 +53,8 @@ public class JourneyMap extends TextProperties {
     protected int offsetY;
 
 
-    private static final List<Component> MINIMAP_TEXT_LIST = new ArrayList<>(3);
+    //private static final List<Component> MINIMAP_TEXT_LIST = new ArrayList<>(3);
     static final JourneymapClient jm = JourneymapClient.getInstance();
-
-/*    public class SeasonLab extends ThemeLabelSource {
-        public static ThemeLabelSource.InfoSlot SEASONNAME = create(MODID, "Season", 100L, 100L, CurrentSeason::getSeasonName);
-    }*/
 
 
     public static boolean jouneymapLoaded () {return ModList.get().isLoaded("journeymap");}
@@ -60,23 +62,11 @@ public class JourneyMap extends TextProperties {
     public static final IGuiOverlay JOURNEYMAP_SEASON = (ForgeGui, seasonStack, partialTick, scaledWidth, scaledHeight) -> {
         JourneyMap map = new JourneyMap();
         if (jouneymapLoaded()) {
-            MINIMAP_TEXT_LIST.clear();
-
-//            boolean biome = FTBChunksClientConfig.MINIMAP_BIOME.get();
-//            boolean xyz = FTBChunksClientConfig.MINIMAP_XYZ.get();
-//
-//            int i = 0;
-//
-//            if(biome){
-//                i++;
-//            }
-//            if(xyz){
-//                i++;
-//            }
+            //INIMAP_TEXT_LIST.clear();
 
             //Season
-            MINIMAP_TEXT_LIST.add(Component.literal(getSeasonName()));
-
+            String[] MINIMAP_TEXT_LIST = new String[3];
+            MINIMAP_TEXT_LIST[0] = getSeasonName();
 
             //Icon chooser
             ResourceLocation SEASON = new ResourceLocation(MODID,
@@ -84,9 +74,6 @@ public class JourneyMap extends TextProperties {
 
             //Values
             Font fontRenderer = mc.font;
-            int height = mc.getWindow().getHeight();
-            int width = mc.getWindow().getWidth();
-            double guiSize = mc.getWindow().getGuiScale();
             MinimapSpec minimapSpec;
             MiniMapProperties miniMapProperties;
 
@@ -97,33 +84,49 @@ public class JourneyMap extends TextProperties {
             float fontScale = jm.getActiveMiniMapProperties().fontScale.get();
             float alpha = jm.getActiveMiniMapProperties().infoSlotAlpha.get();
             Theme.LabelSpec label = new Theme.LabelSpec();
+            int labelColor = ThemeLoader.getCurrentTheme().fullscreen.statusLabel.highlight.getColor();
+            int textColor = ThemeLoader.getCurrentTheme().minimap.square.bottom.getColor();
 
-            float x = jm.getActiveMiniMapProperties().positionX.get();
-            float y = jm.getActiveMiniMapProperties().positionY.get();
+            double guiSize = mc.getWindow().getGuiScale();
+            int width = mc.getWindow().getWidth();
+            int height = mc.getWindow().getHeight();
+
+
+            float posX = jm.getActiveMiniMapProperties().positionX.get();
+            float posY = jm.getActiveMiniMapProperties().positionY.get();
+
+
+            float x = width*posX;
+            float y = height*posY;
+
+
+            int infoLabelCount = 2;//Will be count of how many labels are currently on
 
             int labelHeight = (int)((double)(DrawUtil.getLabelHeight(fontRenderer, label.shadow)) * (fontScale));
+            int labelWidth = mc.font.width(Arrays.toString(MINIMAP_TEXT_LIST));
+            double vpad = fontRenderer.isBidirectional() ? 0.0 : (fontShadow ? 6.0 : 4.0);
+            double bgHeight = (labelHeight * infoLabelCount)+(vpad/2)+1;//1 is to offset for lack of background alpha in size
 
-            /*Todo
-                * Need to find margins and frame size for map
-                    * Also margin from map to first
-                * Need to make int for how many labels are displayed currently (x out of 4)
-                    * labelHeight should be correct size
-                * Setup if statement for each of top left and top right (on the drawShadow)
-                * Add icon again next to season
-                * Add background alpha color (float alpha?)
+            MINIMAP_TEXT_LIST[1] = String.valueOf(labelHeight);
+            MINIMAP_TEXT_LIST[2] = String.valueOf(vpad);
 
-             */
+            float labelX = (x+((float)minimapSize/2));
+            float labelY = (y+minimapSize)+(float)(bgHeight);
 
-            if (!MINIMAP_TEXT_LIST.isEmpty()) {
+
+
+
+            if (!(MINIMAP_TEXT_LIST.length == 0)) {
                 seasonStack.pushPose();
                 //seasonStack.translate(0,0,0);
                 seasonStack.scale(fontScale/(float)guiSize, fontScale/(float)guiSize, 1.0F);
-                for (Component s : MINIMAP_TEXT_LIST) {
-                    mc.font.drawShadow(seasonStack, String.valueOf(scaledHeight), (float) ((width)-(minimapSize/2)), (float) ((0)+(minimapSize)), -1);
-                }
+                //for (Component s : MINIMAP_TEXT_LIST) {
+                //mc.font.drawShadow(seasonStack, Arrays.toString(MINIMAP_TEXT_LIST), labelX, labelY, -1);
+                DrawUtil.drawLabel(seasonStack, Arrays.toString(MINIMAP_TEXT_LIST), (double)labelX, (double)labelY, DrawUtil.HAlign.Center, DrawUtil.VAlign.Below, labelColor, alpha, textColor, alpha, fontScale, fontShadow);
+               // }
                 int iconDim = mc.font.lineHeight;
                 int offsetDim = 1;
-                MINIMAP_TEXT_LIST.clear();
+                //MINIMAP_TEXT_LIST.clear();
 
 
                 RenderSystem.setShader(GameRenderer::getPositionTexShader);
@@ -137,3 +140,12 @@ public class JourneyMap extends TextProperties {
     };
 }
 
+/*Todo
+ * Need to make int for how many labels are displayed currently (x out of 4)
+ * Setup if statement for each of top left and top right (on the drawShadow)
+ * Add icon again next to season
+ * incorporate "scale" for if minimap size is changed?
+ * Add background alpha color (float alpha?)
+ * Need to figure out how to get color from theme????
+
+ */
