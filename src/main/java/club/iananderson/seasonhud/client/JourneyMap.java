9d462c63318325d42062a2f630e762236cb3d5eb
Ian Anderson <iananderson92@gmail.com>
2022-11-17 17:09:07 -0700

    continued JourneyMap

--- src/main/java/club/iananderson/seasonhud/client/JourneyMap.java
+++ src/main/java/club/iananderson/seasonhud/client/JourneyMap.java
@@ -14,9 +14,11 @@ import journeymap.client.render.draw.DrawUtil;
 import journeymap.client.ui.UIManager;
 import journeymap.client.ui.component.Label;
 import journeymap.client.ui.minimap.DisplayVars;
+import journeymap.client.ui.minimap.Position;
 import journeymap.client.ui.theme.Theme;
 import journeymap.client.ui.theme.Theme.Minimap.MinimapSpec;
 import journeymap.client.ui.theme.ThemeLabelSource;
+import journeymap.client.ui.theme.ThemeMinimapFrame;
 import journeymap.client.ui.theme.impl.FlatTheme;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
@@ -74,46 +76,64 @@ public class JourneyMap extends TextProperties {
 
             //Values
             Font fontRenderer = mc.font;
-            MinimapSpec minimapSpec;
-            MiniMapProperties miniMapProperties;
 
 
-            int minimapSize = jm.getActiveMiniMapProperties().getSize();
+            int minimapWidth;
+            int minimapHeight;
+            int minimapSize = minimapWidth= minimapHeight= jm.getActiveMiniMapProperties().getSize();
+            int halfWidth = minimapHeight/2;
+            int halfHeight = minimapWidth/2;
+
 
             boolean fontShadow = map.hasFontShadow();
             float fontScale = jm.getActiveMiniMapProperties().fontScale.get();
             float alpha = jm.getActiveMiniMapProperties().infoSlotAlpha.get();
             Theme.LabelSpec label = new Theme.LabelSpec();
+            Theme theme = new Theme();
             int labelColor = ThemeLoader.getCurrentTheme().fullscreen.statusLabel.highlight.getColor();
             int textColor = ThemeLoader.getCurrentTheme().minimap.square.bottom.getColor();
 
             double guiSize = mc.getWindow().getGuiScale();
-            int width = mc.getWindow().getWidth();
-            int height = mc.getWindow().getHeight();
+            int screenWidth = mc.getWindow().getWidth();
+            int screenHeight = mc.getWindow().getHeight();
 
 
             float posX = jm.getActiveMiniMapProperties().positionX.get();
             float posY = jm.getActiveMiniMapProperties().positionY.get();
 
 
-            float x = width*posX;
-            float y = height*posY;
+            int textureX = screenWidth*(int)posX;
+            int textureY = screenHeight*(int)posY;
+            int translateX=0;
+            int translateY=0;
+
+            MinimapSpec minimapSpec = theme.minimap.square;
+            MiniMapProperties miniMapProperties = new MiniMapProperties(jm.getActiveMinimapId());
+            ThemeMinimapFrame minimapFrame = new ThemeMinimapFrame(theme, minimapSpec, miniMapProperties, minimapWidth, minimapHeight);
+
+            int marginX;
+            int marginY = marginX = minimapSpec.margin;
+            double frameWidth = minimapFrame.getWidth();
 
 
-            int infoLabelCount = 2;//Will be count of how many labels are currently on
+            int infoLabelCount = 0;//Will be count of how many labels are currently on
 
             int labelHeight = (int)((double)(DrawUtil.getLabelHeight(fontRenderer, label.shadow)) * (fontScale));
+
+            if (!minimapSpec.labelBottomInside) {
+                marginY += labelHeight;
+            }
+
             int labelWidth = mc.font.width(Arrays.toString(MINIMAP_TEXT_LIST));
             double vpad = fontRenderer.isBidirectional() ? 0.0 : (fontShadow ? 6.0 : 4.0);
             double bgHeight = (labelHeight * infoLabelCount)+(vpad/2)+1;//1 is to offset for lack of background alpha in size
 
-            MINIMAP_TEXT_LIST[1] = String.valueOf(labelHeight);
-            MINIMAP_TEXT_LIST[2] = String.valueOf(vpad);
-
-            float labelX = (x+((float)minimapSize/2));
-            float labelY = (y+minimapSize)+(float)(bgHeight);
+            MINIMAP_TEXT_LIST[1] = String.valueOf(textureX);
+            MINIMAP_TEXT_LIST[2] = String.valueOf(textureY);
 
 
+
+            Position positionName = jm.getActiveMiniMapProperties().position.get();
 
 
             if (!(MINIMAP_TEXT_LIST.length == 0)) {
@@ -122,8 +142,29 @@ public class JourneyMap extends TextProperties {
                 seasonStack.scale(fontScale/(float)guiSize, fontScale/(float)guiSize, 1.0F);
                 //for (Component s : MINIMAP_TEXT_LIST) {
                 //mc.font.drawShadow(seasonStack, Arrays.toString(MINIMAP_TEXT_LIST), labelX, labelY, -1);
-                DrawUtil.drawLabel(seasonStack, Arrays.toString(MINIMAP_TEXT_LIST), (double)labelX, (double)labelY, DrawUtil.HAlign.Center, DrawUtil.VAlign.Below, labelColor, alpha, textColor, alpha, fontScale, fontShadow);
-               // }
+
+
+               if(positionName == Position.TopRight){
+                   if(!minimapSpec.labelTopInside) {
+                       marginY = Math.max(marginY, labelHeight + 2 * theme.minimap.square.margin);
+                   }
+                   textureX = screenWidth - minimapWidth - marginX;
+                   textureY = screenHeight - minimapHeight - marginY;
+                   translateX = screenWidth / 2 - halfWidth - marginX;
+                   translateY = screenHeight / 2 - halfHeight - marginY;
+
+               } else if (positionName==Position.Custom) {
+               }
+                int startY = textureY + minimapHeight;
+                startY += minimapSpec.labelBottomInside ? -minimapSpec.margin - labelHeight : minimapSpec.margin;
+
+                int centerX = (int)Math.floor((double)(textureX + (minimapWidth / 2)+(labelWidth/2)-(frameWidth)));
+                float labelX = textureX-translateX;
+                float labelY = textureY-translateY;
+                DrawUtil.drawLabel(seasonStack, Arrays.toString(MINIMAP_TEXT_LIST), (double)centerX, (double)labelY, DrawUtil.HAlign.Center, DrawUtil.VAlign.Below, labelColor, alpha, textColor, alpha, fontScale, fontShadow);
+
+
+                // }
                 int iconDim = mc.font.lineHeight;
                 int offsetDim = 1;
                 //MINIMAP_TEXT_LIST.clear();
@@ -141,6 +182,9 @@ public class JourneyMap extends TextProperties {
 }
 
 /*Todo
+ * Fix X position of label
+ * Issue with gui scaling and position
+    * Might be a issue with position. Works fine in custom.
  * Need to make int for how many labels are displayed currently (x out of 4)
  * Setup if statement for each of top left and top right (on the drawShadow)
  * Add icon again next to season
