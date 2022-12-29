f22631d7e842acda2654285b22779cb7f0fa84cf
Ian Anderson <iananderson92@gmail.com>
2022-12-28 23:28:43 -0700

    There were minor changes in the Screen class between 1.19.2 -> 1.19.3, so they will need to be built separately.

--- src/main/java/club/iananderson/seasonhud/event/SeasonHUDScreen.java
+++ src/main/java/club/iananderson/seasonhud/event/SeasonHUDScreen.java
@@ -21,9 +21,6 @@ public class SeasonHUDScreen extends Screen{
     private static final int BUTTON_WIDTH_FULL = 200;
     private static final int BUTTON_WIDTH_HALF = BUTTON_WIDTH_FULL/2;
     private static final int BUTTON_HEIGHT = 20;
-
-    private final int BUTTON_START_Y = MENU_PADDING_FULL;
-    private final int Y_OFFSET = BUTTON_HEIGHT+PADDING;
     public static Screen seasonScreen;
 
     private final Screen lastScreen;
@@ -38,40 +35,41 @@ public class SeasonHUDScreen extends Screen{
         return true;
     }
 
-    public void render(@NotNull PoseStack stack, int mouseX, int mouseY, float partialTicks){
+    @Override
+    public void render(PoseStack stack, int mouseX, int mouseY, float partialTicks){
         this.renderDirtBackground(0);
-        super.render(stack, mouseX, mouseY, partialTicks);
         drawCenteredString(stack, font, title, this.width / 2, PADDING, 16777215);
+        super.render(stack, mouseX, mouseY, partialTicks);
     }
 
+    @Override
     public void init() {
         super.init();
         Minecraft mc = Minecraft.getInstance();
+
         int BUTTON_START_X = (this.width/2) - BUTTON_WIDTH_FULL+PADDING;
+        int BUTTON_START_Y = MENU_PADDING_FULL;
+        int y_OFFSET = BUTTON_HEIGHT + PADDING;
 
         //Buttons
+
         CycleButton<Boolean> showDayButton = CycleButton.onOffBuilder(showDay.get())
-                .create(BUTTON_START_X, BUTTON_START_Y, BUTTON_WIDTH_FULL, BUTTON_HEIGHT,
-                        Component.translatable("menu.seasonhud.button.showDay"),
-                        (b, Off) -> {
-                            Config.setShowDay(Off);
-                        });
+            .create(BUTTON_START_X, BUTTON_START_Y, BUTTON_WIDTH_FULL, BUTTON_HEIGHT,
+            Component.translatable("menu.seasonhud.button.showDay"),
+            (b, Off) -> Config.setShowDay(Off));
+
 
         CycleButton<Boolean> showSubSeasonButton = CycleButton.onOffBuilder(showSubSeason.get())
-                .create(BUTTON_START_X, (BUTTON_START_Y + Y_OFFSET), BUTTON_WIDTH_FULL, BUTTON_HEIGHT,
-                        Component.translatable("menu.seasonhud.button.showSubSeason"),
-                        (b, Off) -> {
-                            Config.setShowSubSeason(Off);
-                        });
+            .create(BUTTON_START_X, (BUTTON_START_Y + y_OFFSET), BUTTON_WIDTH_FULL, BUTTON_HEIGHT,
+            Component.translatable("menu.seasonhud.button.showSubSeason"),
+            (b, Off) -> Config.setShowSubSeason(Off));
 
         ExtendedButton doneButton = new ExtendedButton((this.width/2 + PADDING), (this.height - MENU_PADDING_HALF), BUTTON_WIDTH_HALF, BUTTON_HEIGHT, Component.translatable("gui.done"), b -> {
             mc.options.save();
             mc.setScreen(this.lastScreen);
         });
 
-        ExtendedButton cancelButton = new ExtendedButton((this.width / 2 - (PADDING + BUTTON_WIDTH_HALF)), this.height - MENU_PADDING_HALF, BUTTON_WIDTH_HALF, BUTTON_HEIGHT, Component.translatable("gui.cancel"), b -> {
-            mc.setScreen(this.lastScreen);
-        });
+        ExtendedButton cancelButton = new ExtendedButton((this.width / 2 - (PADDING + BUTTON_WIDTH_HALF)), this.height - MENU_PADDING_HALF, BUTTON_WIDTH_HALF, BUTTON_HEIGHT, Component.translatable("gui.cancel"), b -> mc.setScreen(this.lastScreen));
 
         addRenderableWidget(showDayButton);
         addRenderableWidget(showSubSeasonButton);
