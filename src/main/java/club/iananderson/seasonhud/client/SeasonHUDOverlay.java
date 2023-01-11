6038a5c7e327c595ca2b5442bc4bd2be631dc5b1
Ian Anderson <iananderson92@gmail.com>
2023-01-10 21:38:44 -0700

    Added config option to offset default hud

--- src/main/java/club/iananderson/seasonhud/client/SeasonHUDOverlay.java
+++ src/main/java/club/iananderson/seasonhud/client/SeasonHUDOverlay.java
@@ -2,13 +2,14 @@ package club.iananderson.seasonhud.client;
 
 import club.iananderson.seasonhud.SeasonHUD;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
 
-import static club.iananderson.seasonhud.config.Config.enableMod;
+import static club.iananderson.seasonhud.config.Config.*;
 import static club.iananderson.seasonhud.data.CurrentSeason.*;
 import static club.iananderson.seasonhud.client.minimaps.FTBChunks.ftbChunksLoaded;
 import static club.iananderson.seasonhud.client.minimaps.JourneyMap.journeymapLoaded;
@@ -17,11 +18,14 @@ import static club.iananderson.seasonhud.client.minimaps.XaeroMinimap.minimapLoa
 //HUD w/ no minimap installed
 public class SeasonHUDOverlay {
     public static final IGuiOverlay HUD_SEASON = (ForgeGui, seasonStack, partialTick, screenWidth, screenHeight) -> {
+        Minecraft mc = Minecraft.getInstance();
+        float guiSize = (float) mc.getWindow().getGuiScale();
 
-        int x = 0;
-        int y = 0;
+        int x = (int) (hudX.get()/guiSize);
+        int y = (int) ((hudY.get())/guiSize);
         int iconDim = 10;
         int offsetDim = 5;
+
 
         ResourceLocation SEASON;
 
@@ -43,7 +47,9 @@ public class SeasonHUDOverlay {
             seasonStack.scale(1F, 1F, 1F);
 
             //Text
-            ForgeGui.getFont().drawShadow(seasonStack, getSeasonName().get(0), (float) (x + iconDim + offsetDim + 2), (float) (y + offsetDim + (.12 * iconDim)), 0xffffffff);
+            float textX = (x + iconDim + offsetDim + 2);
+            float textY = (float)(y + offsetDim + (.12 * iconDim));
+            ForgeGui.getFont().drawShadow(seasonStack, getSeasonName().get(0),textX, textY, 0xffffffff);
 
             //Icon
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
