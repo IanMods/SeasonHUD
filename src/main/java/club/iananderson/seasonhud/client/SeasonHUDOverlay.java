671370aed26670538c6c31c9e5e16ad4d81a57b7
Ian Anderson <iananderson92@gmail.com>
2023-01-11 17:06:16 -0700

    Started work on additional config options for when no minimap is installed

--- src/main/java/club/iananderson/seasonhud/client/SeasonHUDOverlay.java
+++ src/main/java/club/iananderson/seasonhud/client/SeasonHUDOverlay.java
@@ -1,13 +1,17 @@
 package club.iananderson.seasonhud.client;
 
 import club.iananderson.seasonhud.SeasonHUD;
+import club.iananderson.seasonhud.config.Config;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
+
+import java.util.ArrayList;
 
 import static club.iananderson.seasonhud.config.Config.*;
 import static club.iananderson.seasonhud.data.CurrentSeason.*;
@@ -21,10 +25,18 @@ public class SeasonHUDOverlay {
         Minecraft mc = Minecraft.getInstance();
         float guiSize = (float) mc.getWindow().getGuiScale();
 
-        int x = (int) (hudX.get()/guiSize);
-        int y = (int) ((hudY.get())/guiSize);
-        int iconDim = 10;
-        int offsetDim = 5;
+        int xOffset = (int) (hudX.get()/guiSize);
+        int yOffset = (int) ((hudY.get())/guiSize);
+        int x = 1;
+        int y = 1;
+
+        int stringHeight = (mc.font.lineHeight)+1;
+        int iconDim = stringHeight+3;
+        int offsetDim = 2;
+
+        ArrayList<Component> seasonName = getSeasonName();
+        int stringWidth = mc.font.width(seasonName.get(0)) + iconDim + offsetDim;// might need to take offsetDim out
+
 
 
         ResourceLocation SEASON;
@@ -43,19 +55,45 @@ public class SeasonHUDOverlay {
         }
 
         if (!minimapLoaded() && !ftbChunksLoaded() && !journeymapLoaded() && enableMod.get()) {
+            Location hudLoc = hudLocation.get();
+            switch (hudLoc) {
+                case TOP_LEFT -> {
+                    x = 0;
+                    y = 0;
+                }
+                case TOP_CENTER -> {
+                    x = screenWidth / 2 - stringWidth / 2;
+                    y = 0;
+                }
+                case TOP_RIGHT -> {
+                    x = screenWidth - stringWidth;
+                    y = 0;
+                }
+                case BOTTOM_LEFT -> {
+                    x = 0;
+                    y = screenHeight - stringHeight;
+                }
+                case BOTTOM_RIGHT -> {
+                    x = screenWidth - stringWidth;
+                    y = screenHeight - stringHeight;
+                }
+            }
+
             seasonStack.pushPose();
             seasonStack.scale(1F, 1F, 1F);
 
             //Text
-            float textX = (x + iconDim + offsetDim + 2);
-            float textY = (float)(y + offsetDim + (.12 * iconDim));
-            ForgeGui.getFont().drawShadow(seasonStack, getSeasonName().get(0),textX, textY, 0xffffffff);
+            int iconX = x + xOffset + offsetDim;
+            int iconY = y + yOffset + offsetDim;
+            float textX = (iconX + iconDim+offsetDim);
+            float textY =(iconY+(iconDim-stringHeight)); //double check this is exact
+            ForgeGui.getFont().drawShadow(seasonStack, seasonName.get(0),textX, textY, 0xffffffff);
 
             //Icon
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
             RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
             RenderSystem.setShaderTexture(0, SEASON);
-            GuiComponent.blit(seasonStack, x + offsetDim, y + offsetDim, 0, 0, iconDim, iconDim, iconDim, iconDim);
+            GuiComponent.blit(seasonStack, iconX, iconY, 0, 0, iconDim, iconDim, iconDim, iconDim);
             seasonStack.popPose();
         }
     };
--- src/main/java/club/iananderson/seasonhud/config/Config.java
+++ src/main/java/club/iananderson/seasonhud/config/Config.java
@@ -13,9 +13,17 @@ public class Config {
         setupConfig(BUILDER);
         GENERAL_SPEC = BUILDER.build();
     }
+    public enum Location{
+        TOP_LEFT,
+        TOP_CENTER,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT
+    }
     public static ForgeConfigSpec.BooleanValue enableMod;
     public static ForgeConfigSpec.ConfigValue<Integer> hudX;
     public static ForgeConfigSpec.ConfigValue<Integer> hudY;
+    public static ForgeConfigSpec.ConfigValue<Location> hudLocation;
     public static ForgeConfigSpec.BooleanValue showSubSeason;
     public static ForgeConfigSpec.BooleanValue showDay;
 
@@ -28,14 +36,6 @@ public class Config {
                 .comment("Enable the mod? \n (true/false)")
                 .define("enable_mod",true);
 
-        showSubSeason = BUILDER
-                .comment("Show sub-season (i.e. Early Winter, Late Autumn) instead of basic season? \n (true/false)")
-                .define("enable_show_sub_season",true);
-
-        showDay = BUILDER
-                .comment("Show the current day of the season/sub-season? \n (true/false)")
-                .define("enable_show_day",true);
-
         hudX = BUILDER
                 .comment("The horizontal offset of the HUD when no minimap is installed (in pixels)\n Default is 0")
                 .define("hud_x_position",0);
@@ -43,6 +43,18 @@ public class Config {
         hudY = BUILDER
                 .comment("The vertical offset of the HUD when no minimap is installed (in pixels)\n Default is 0")
                 .define("hud_y_position",0);
+
+        hudLocation = BUILDER
+                .comment("Part of the screen to display the HUD when no minimap is installed\n Default is TOP_LEFT")
+                .defineEnum("hud_location",Location.TOP_LEFT);
+
+        showSubSeason = BUILDER
+                .comment("Show sub-season (i.e. Early Winter, Late Autumn) instead of basic season? \n (true/false)")
+                .define("enable_show_sub_season",true);
+
+        showDay = BUILDER
+                .comment("Show the current day of the season/sub-season? \n (true/false)")
+                .define("enable_show_day",true);
 
         BUILDER.pop();
         BUILDER.pop();
@@ -60,6 +72,10 @@ public class Config {
     public static void setHudY(int hudY) {
         Config.hudX.set(hudY);
     }
+    public static void setHudLocation(Location hudLocation) {
+        Config.hudLocation.set(hudLocation);
+    }
+
 
     public static void setEnableMod(boolean enableMod) {
         Config.enableMod.set(enableMod);
