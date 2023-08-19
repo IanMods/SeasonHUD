2bf8cf70eb386c87e1ce1a138b195ca5b8c4742e
Ian Anderson <iananderson92@gmail.com>
2023-08-19 14:36:42 -0700

    Fabric JourneyMap support

--- fabric/src/main/java/club/iananderson/seasonhud/impl/minimaps/HiddenMinimap.java
+++ fabric/src/main/java/club/iananderson/seasonhud/impl/minimaps/HiddenMinimap.java
@@ -8,9 +8,9 @@ import static club.iananderson.seasonhud.impl.minimaps.CurrentMinimap.loadedMini
 
 public class HiddenMinimap {
     public static boolean minimapHidden(){
-//        if (loadedMinimap("journeymap")) {
-//            return !JourneymapClient.getInstance().getActiveMiniMapProperties().enabled.get();
-//        }
+        if (loadedMinimap("journeymap")) {
+            return !JourneymapClient.getInstance().getActiveMiniMapProperties().enabled.get();
+        }
         if (loadedMinimap("ftbchunks")) {
             return !FTBChunksClientConfig.MINIMAP_ENABLED.get();
         }
