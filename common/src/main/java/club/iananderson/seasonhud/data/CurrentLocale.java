a892e099f64d588fc8a9611aa75de8b88eddc353
Ian Anderson <iananderson92@gmail.com>
2023-09-04 07:46:30 -0700

    ## [1.5.5] - 2023-09-04
    ### Added
    - Added Russian translation provided by mpustovoi

--- common/src/main/java/club/iananderson/seasonhud/data/CurrentLocale.java
+++ common/src/main/java/club/iananderson/seasonhud/data/CurrentLocale.java
@@ -19,6 +19,7 @@ public class CurrentLocale {
         language.add("en_gb");
         language.add("en_nz");
         language.add("en_us");
+        language.add("ru_ru");
         language.add("zh_cn");
         return language;
     }
