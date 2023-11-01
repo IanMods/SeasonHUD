a344caea1dcf59bbba16bb13089cc576a9b6c12b
IanAnderson <iananderson92@gmail.com>
2023-11-01 02:20:26 -0700

    Working on fixing alignment with hidden effects Corrected initial alignment first

--- forge/src/main/java/club/iananderson/seasonhud/client/minimaps/XaeroMinimap.java
+++ forge/src/main/java/club/iananderson/seasonhud/client/minimaps/XaeroMinimap.java
@@ -11,11 +11,13 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import xaero.common.XaeroMinimapSession;
 import xaero.common.core.XaeroMinimapCore;
 import xaero.common.gui.IScreenBase;
 import xaero.common.interfaces.pushbox.PotionEffectsPushBox;
 import xaero.common.minimap.MinimapInterface;
 import xaero.common.minimap.MinimapProcessor;
+import xaero.common.minimap.info.InfoDisplay;
 import xaero.common.minimap.info.InfoDisplayManager;
 import xaero.common.minimap.waypoints.WaypointsManager;
 import xaero.common.settings.ModSettings;
@@ -23,6 +25,7 @@ import xaero.common.settings.ModSettings;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 import static club.iananderson.seasonhud.impl.minimaps.CurrentMinimap.loadedMinimap;
 import static club.iananderson.seasonhud.impl.minimaps.HiddenMinimap.minimapHidden;
@@ -44,10 +47,11 @@ public class XaeroMinimap {
             ResourceLocation dim = Objects.requireNonNull(mc.level).dimension().location();
 
             ModSettings modSettings = modMain.getSettings();
+            XaeroMinimapSession currentSession = XaeroMinimapCore.currentSession;
             MinimapInterface minimapInterface = modMain.getInterfaces().getMinimapInterface();
-            MinimapProcessor minimapProcessor = XaeroMinimapCore.currentSession.getMinimapProcessor();
+            MinimapProcessor minimapProcessor = currentSession.getMinimapProcessor();
             InfoDisplayManager infoDisplayManager = minimapInterface.getInfoDisplayManager();
-            WaypointsManager waypointsManager = XaeroMinimapCore.currentSession.getWaypointsManager();
+            WaypointsManager waypointsManager = currentSession.getWaypointsManager();
             PotionEffectsPushBox potionPushBox = modMain.getInterfaces().normalPotionEffectsPushBox;
             Collection<MobEffectInstance> potionEffect = Objects.requireNonNull(mc.player).getActiveEffects();
 
@@ -80,21 +84,27 @@ public class XaeroMinimap {
 
             //Icon
             double scale = mc.getWindow().getGuiScale();
+            int screenWidth = mc.getWindow().getScreenWidth();
+            int screenHeight = mc.getWindow().getScreenHeight();
+
+            float minimapScale = modSettings.getMinimapScale();
+            int xaeroWidth = (int) (screenWidth/minimapScale);
 
             float stringWidth = mc.font.width(underText.get(0));
             float stringHeight = (mc.font.lineHeight);
-            float mapSize = modSettings.getMinimapSize();
 
-            float minimapScale = modSettings.getMinimapScale();
-            float mapScale = ((float) (scale / (double) minimapScale));
-            float fontScale = 1 / mapScale;
+            float mapScale = ((float) (scale / minimapScale));
+            int minimapSize = modSettings.getMinimapSize();
+            double scaledMinimapSize = minimapSize * minimapScale;
 
             int iconDim = (int) stringHeight;
-            int size = (int) mapSize;
-            int framesize = 4;
+            int minimapFrameSize = (int) (4);
+            int minimapPadding = (int) (5);
             int scaledHeight = (int) ((float) height * mapScale);
             int align = modSettings.minimapTextAlign;
-            int yOffset = (int) (((framesize * 2) + 9) + (filteredIndexSeason * (stringHeight + 1)));
+
+            int offset = (minimapFrameSize *2)+(minimapPadding*2);
+            int totalOffsetY = (int) (offset + (filteredIndexSeason * (stringHeight + 0.5)));
 
             float x = minimapInterface.getX();
             float y = minimapInterface.getY();
@@ -102,26 +112,37 @@ public class XaeroMinimap {
             float scaledY = (y * mapScale);
 
             int potionY = 0;
-            int potionPushBoxHeight = potionPushBox.getH(width,height);
-            int potionPushBoxWidth = potionPushBox.getW(width,height);
+            int potionPushBoxWidth = potionPushBox.getW(screenWidth,screenHeight);
+            int potionPushBoxHeight = 0;
 
             if(potionPushBox.isActive() && mc.player != null && !potionEffect.isEmpty()){
-                if(((y - potionPushBoxHeight) < 0) && ((x + mapSize) > width-potionPushBoxWidth)){
-                    scaledY = 0;
-                    potionY = (int) (potionPushBoxHeight * mapScale);
+                potionPushBoxHeight = potionPushBox.getH(width,height);
+
+                //Todo
+                // ((scaledX + offset + minimapSize)* minimapScale) is correct
+                // -- ~2413 pixels until it moves
+                // (screenWidth-(potionPushBoxWidth*scale) is about 3 pixels off
+                // -- Reads ~2407 pixels when it moves
+                // -- Should be ~2410 so about 3 pixels off
+
+                //Todo #2
+                // Look into hidden effects and how to detect them
+
+                if((y <= potionPushBoxHeight) && (((scaledX + offset + minimapSize)* minimapScale) > (screenWidth-(potionPushBoxWidth*scale)))){
+                    scaledY = potionPushBoxHeight * mapScale;
                 }
             }
 
-            boolean under = ((int) scaledY + size / 2) < scaledHeight / 2;
+            boolean under = ((int) scaledY + minimapSize / 2) < scaledHeight / 2;
 
-            int stringX = (int) (scaledX + (align == 0 ? size / 2 - stringWidth / 2 + iconDim + 1 : (align == 1 ? 6 : iconDim + size - stringWidth + framesize)));
-            int stringY = (int) ((scaledY) + (under ? size + yOffset : -yOffset + 7));
+            int stringX = (int) (scaledX + (align == 0 ? minimapSize / 2 - stringWidth / 2 + iconDim + 0.5 : (align == 1 ? 6 : iconDim + minimapSize - stringWidth + minimapFrameSize)));
+            int stringY = (int) ((scaledY) + (under ? minimapSize+totalOffsetY : -totalOffsetY + 7));
 
             //Icon Draw
             if (!minimapHidden() && (!modSettings.hideMinimapUnderScreen || mc.screen == null || mc.screen instanceof IScreenBase || mc.screen instanceof ChatScreen || mc.screen instanceof DeathScreen)
                     && (!modSettings.hideMinimapUnderF3 || !mc.options.renderDebug)) {
                 seasonStack.pushPose();
-                seasonStack.scale(fontScale, fontScale, 1.0F);
+                seasonStack.scale(1.0F / mapScale, 1.0F / mapScale, 1.0F);
 
                 //Icon
                 ResourceLocation SEASON = getSeasonResource();
