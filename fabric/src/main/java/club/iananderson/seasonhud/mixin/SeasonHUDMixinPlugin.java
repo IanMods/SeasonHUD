e1deddc902e44ba873d9d603b07a90ac173d46fc
IanAnderson <iananderson92@gmail.com>
2023-11-13 19:41:36 -0700

    Fixed Fabric Mixins

--- fabric/src/main/java/club/iananderson/seasonhud/mixin/SeasonHUDMixinPlugin.java
+++ fabric/src/main/java/club/iananderson/seasonhud/mixin/SeasonHUDMixinPlugin.java
@@ -1,16 +1,33 @@
 package club.iananderson.seasonhud.mixin;
 
+import club.iananderson.seasonhud.platform.FabricPlatformHelper;
+import com.google.common.collect.ImmutableMap;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 
 public class SeasonHUDMixinPlugin implements IMixinConfigPlugin {
 
     public SeasonHUDMixinPlugin(){
 
+    }
+
+    private static final FabricPlatformHelper fabricPlatformHelper = new FabricPlatformHelper();
+
+    private static final Supplier<Boolean> TRUE = () -> true;
+
+    private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
+            "club.iananderson.seasonhud.mixin.XaeroBuiltInInfoDisplays", () -> (fabricPlatformHelper.isModLoaded("xaerominimap") || fabricPlatformHelper.isModLoaded("xaerominimapfair"))
+    );
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
     }
 
     @Override
@@ -21,11 +38,6 @@ public class SeasonHUDMixinPlugin implements IMixinConfigPlugin {
     @Override
     public String getRefMapperConfig() {
         return null;
-    }
-
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return true;
     }
 
     public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
