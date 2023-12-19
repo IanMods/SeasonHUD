4bdeeac5894fcda892915ae19fbaea3756f6a215
IanAnderson <iananderson92@gmail.com>
2023-12-18 19:32:10 -0700

    Fix for MapAtlas icon

--- forge/src/main/java/club/iananderson/seasonhud/client/minimaps/MapAtlases.java
+++ forge/src/main/java/club/iananderson/seasonhud/client/minimaps/MapAtlases.java
@@ -1,6 +1,7 @@
 package club.iananderson.seasonhud.client.minimaps;
 
 import club.iananderson.seasonhud.config.Config;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
@@ -25,7 +26,24 @@ public class MapAtlases implements IGuiOverlay{
     protected final int BG_SIZE = 64;
     private final Minecraft mc = Minecraft.getInstance();
 
-    /* Todo test this */
+    private static void drawStringWithLighterShadow(GuiGraphics context, Font font, MutableComponent text, int x, int y) {
+        context.drawString(font, text, x + 1, y + 1, 5855577, false);
+        context.drawString(font, text, x, y, 14737632, false);
+    }
+
+    public static void drawScaledComponent(GuiGraphics context, Font font, int x, int y, MutableComponent text, float textScaling, int maxWidth, int targetWidth) {
+        PoseStack pose = context.pose();
+        float textWidth = (float)font.width(text);
+        float scale = Math.min(1.0F, (float)maxWidth * textScaling / textWidth);
+        scale *= textScaling;
+        float centerX = (float)x + (float)targetWidth / 2.0F;
+        pose.pushPose();
+        pose.translate(centerX, (float)(y + 4), 5.0F);
+        pose.scale(scale, scale, 1.0F);
+        pose.translate(-textWidth / 2.0F, -4.0F, 0.0F);
+        drawStringWithLighterShadow(context, font, text, 0, 0);
+        pose.popPose();
+    }
 
     public static void drawMapComponentSeason(GuiGraphics poseStack, Font font, int x, int y, int targetWidth, float textScaling) {
         if (loadedMinimap("map_atlases")) {
@@ -35,7 +53,7 @@ public class MapAtlases implements IGuiOverlay{
 
             float globalScale = (float)(double)MapAtlasesClientConfig.miniMapScale.get();
             //String seasonToDisplay = getSeasonName().get(0).getString();
-            drawScaledComponent(poseStack, font, x, y, seasonCombined.getString(), textScaling / globalScale, targetWidth, (int)(targetWidth / globalScale));
+            drawScaledComponent(poseStack, font, x, y, seasonCombined, textScaling / globalScale, targetWidth, (int)(targetWidth / globalScale));
         }
     }
 
