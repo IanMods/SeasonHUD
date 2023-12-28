12d3ba04af0a548a757f4c359a9279e444c2411c
IanAnderson <iananderson92@gmail.com>
2023-12-28 06:54:23 -0700

    Made the config file more organized

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
+## [1.7.7] - 2023-12-28
+
+### Changed
+- Made the config file more organized. You may need to reapply any changes you made to it previously.
+
+
 ## [1.7.6] - 2023-12-26
 
 ### Fixed
--- common/src/main/java/club/iananderson/seasonhud/config/Config.java
+++ common/src/main/java/club/iananderson/seasonhud/config/Config.java
@@ -15,107 +15,139 @@ public class Config {
     }
 
     public static ForgeConfigSpec.BooleanValue enableMod;
-    public static ForgeConfigSpec.BooleanValue needCalendar;
+    public static ForgeConfigSpec.ConfigValue<Location> hudLocation;
     public static ForgeConfigSpec.ConfigValue<Integer> hudX;
     public static ForgeConfigSpec.ConfigValue<Integer> hudY;
-    public static ForgeConfigSpec.ConfigValue<Location> hudLocation;
+    public static ForgeConfigSpec.BooleanValue needCalendar;
+    public static ForgeConfigSpec.BooleanValue showTropicalSeason;
     public static ForgeConfigSpec.BooleanValue showSubSeason;
     public static ForgeConfigSpec.BooleanValue showDay;
-    public static ForgeConfigSpec.BooleanValue showTropicalSeason;
     public static ForgeConfigSpec.BooleanValue showMinimapHidden;
     public static ForgeConfigSpec.BooleanValue journeyMapAboveMap;
     public static ForgeConfigSpec.BooleanValue journeyMapMacOS;
 
-
-
-
     private static void setupConfig(ForgeConfigSpec.Builder BUILDER){
-        BUILDER.push("Configs for SeasonHUD");
-        BUILDER.push("HUD:");
+        BUILDER.push("SeasonHUD");
+            enableMod = BUILDER
+                    .comment("""
+                            Enable the mod?
+                            (true/false)
+                            Default is true.""")
+                    .define("enable_mod",true);
 
-        enableMod = BUILDER
-                .comment("Enable the mod? \n (true/false)")
-                .define("enable_mod",true);
+            BUILDER.push("HUD");
+                hudLocation = BUILDER
+                        .comment("""
+                                Part of the screen to display the HUD when no minimap is installed
+                                Default is "TOP_LEFT".""")
+                        .defineEnum("hud_location",Location.TOP_LEFT);
+                hudX = BUILDER
+                        .comment("""
+                                The horizontal offset of the HUD when no minimap is installed (in pixels)
+                                Default is 0.""")
+                        .define("hud_x_position",0);
+                hudY = BUILDER
+                        .comment("""
+                                The vertical offset of the HUD when no minimap is installed (in pixels)
+                                Default is 0.""")
+                        .define("hud_y_position",0);
+            BUILDER.pop();
 
-        needCalendar = BUILDER
-                .comment("Require the Calender item to be in the players inventory to show the HUD? \n (true/false)")
-                .define("need_calendar",false);
+            BUILDER.push("Season");
+                needCalendar = BUILDER
+                        .comment("""
+                                Require the Calender item to be in the players inventory to show the HUD?
+                                (true/false)
+                                Default is false.""")
+                        .define("need_calendar",false);
+                showTropicalSeason = BUILDER
+                        .comment("""
+                                Show the Tropical seasons (Wet/Dry) in Tropical Biomes.
+                                Will not change the season behavior in the biomes.
+                                (true/false)
+                                Default is true.""")
+                        .define("enable_show_tropical_season",true);
+                showSubSeason = BUILDER
+                        .comment("""
+                                Show sub-season (i.e. Early Winter, Mid Autumn, Late Spring) instead of basic season?
+                                (true/false)
+                                Default is true.""")
+                        .define("enable_show_sub_season",true);
 
-        hudX = BUILDER
-                .comment("The horizontal offset of the HUD when no minimap is installed (in pixels)\n Default is 0")
-                .define("hud_x_position",0);
+                showDay = BUILDER
+                        .comment("""
+                                Show the current day of the season/sub-season?
+                                (true/false)
+                                Default is true.""")
+                        .define("enable_show_day",true);
+            BUILDER.pop();
 
-        hudY = BUILDER
-                .comment("The vertical offset of the HUD when no minimap is installed (in pixels)\n Default is 0")
-                .define("hud_y_position",0);
+            BUILDER.push("Minimap");
+                showMinimapHidden = BUILDER
+                            .comment("""
+                                    Show the default SeasonHUD display when the minimap is hidden.
+                                    (true/false)
+                                    Default is false.""")
+                            .define("enable_show_minimap_hidden",false);
 
-        hudLocation = BUILDER
-                .comment("Part of the screen to display the HUD when no minimap is installed\n Default is TOP_LEFT")
-                .defineEnum("hud_location",Location.TOP_LEFT);
-
-        showSubSeason = BUILDER
-                .comment("Show sub-season (i.e. Early Winter, Late Autumn) instead of basic season? \n (true/false)")
-                .define("enable_show_sub_season",true);
-
-        showDay = BUILDER
-                .comment("Show the current day of the season/sub-season? \n (true/false)")
-                .define("enable_show_day",true);
-
-        showTropicalSeason = BUILDER
-                .comment("Show the Tropical seasons (Wet/Dry) in Tropical Biomes" +
-                        "\n Will not change the season behavior"+
-                        "\n (true/false)")
-                .define("enable_show_tropical_season",true);
-
-        showMinimapHidden = BUILDER
-                .comment("Show the default SeasonHUD when the Minimap is hidden \n (true/false)")
-                .define("enable_show_minimap_hidden",false);
-
-        journeyMapAboveMap = BUILDER
-                .comment("Show above the Journeymap map, instead of below. \n (true/false)")
-                .define("enable_above_map",false);
-
-        journeyMapMacOS = BUILDER
-                .comment("Toggle for macOS retina display scaling. \n (true/false)")
-                .define("enable_macOS",false);
-
+                BUILDER.push("Journeymap");
+                    journeyMapAboveMap = BUILDER
+                            .comment("""
+                                    Show above the JourneyMap minimap, instead of below.
+                                    (true/false)
+                                    Default is false.""")
+                            .define("enable_above_map",false);
+                    journeyMapMacOS = BUILDER
+                            .comment("""
+                                    Toggle for macOS retina display scaling when using JourneyMap.
+                                    Enable if the season line is rendering around the halfway point of the screen.
+                                    (true/false)
+                                    Default is false.""")
+                            .define("enable_macOS",false);
+                BUILDER.pop();
+            BUILDER.pop();
         BUILDER.pop();
-        BUILDER.pop();
+    }
+    //SeasonHUD
+    public static void setEnableMod(boolean enableMod) {
+        Config.enableMod.set(enableMod);
     }
 
-    public static void setShowSubSeason(boolean showSubSeason) {
-        Config.showSubSeason.set(showSubSeason);
+    //HUD
+    public static void setHudLocation(Location location) {
+        Config.hudLocation.set(location);
     }
-    public static void setShowDay(boolean showDay) {
-        Config.showDay.set(showDay);
-    }
-    public static void setShowTropicalSeason(boolean showTropicalSeason) {
-        Config.showTropicalSeason.set(showTropicalSeason);
-    }
-
     public static void setHudX(int hudX) {
         Config.hudX.set(hudX);
     }
     public static void setHudY(int hudY) {
         Config.hudY.set(hudY);
     }
-    public static void setHudLocation(Location location) {
-        Config.hudLocation.set(location);
-    }
-    public static void setEnableMod(boolean enableMod) {
-        Config.enableMod.set(enableMod);
-    }
+
+    //Season
     public static void setNeedCalendar(boolean needCalendar) {
         Config.needCalendar.set(needCalendar);
     }
+    public static void setShowTropicalSeason(boolean showTropicalSeason) {
+        Config.showTropicalSeason.set(showTropicalSeason);
+    }
+    public static void setShowSubSeason(boolean showSubSeason) {
+        Config.showSubSeason.set(showSubSeason);
+    }
+    public static void setShowDay(boolean showDay) {
+        Config.showDay.set(showDay);
+    }
+
+    //Minimap
     public static void setShowMinimapHidden(boolean showMinimapHidden) {
         Config.showMinimapHidden.set(showMinimapHidden);
     }
-    public static void setJourneyMapAboveMap(boolean journeyMapAboveMap) {
-        Config.journeyMapAboveMap.set(journeyMapAboveMap);
-    }
-    public static void setJourneyMapMacOS(boolean journeyMapMacOS) {
-        Config.journeyMapMacOS.set(journeyMapMacOS);
-    }
 
+        //Journeymap
+        public static void setJourneyMapAboveMap(boolean journeyMapAboveMap) {
+            Config.journeyMapAboveMap.set(journeyMapAboveMap);
+        }
+        public static void setJourneyMapMacOS(boolean journeyMapMacOS) {
+            Config.journeyMapMacOS.set(journeyMapMacOS);
+        }
 }
