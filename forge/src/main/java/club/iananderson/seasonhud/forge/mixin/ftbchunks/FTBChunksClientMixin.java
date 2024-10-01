35061ed6b346d0922b71434fd74a2b12fd2cc5c9
Ian Anderson <lanAnderson@users.noreply.github.com>
2024-09-30 22:16:46 -0700

    1.16.5 changes

--- forge/src/main/java/club/iananderson/seasonhud/forge/mixin/ftbchunks/FTBChunksClientMixin.java
+++ forge/src/main/java/club/iananderson/seasonhud/forge/mixin/ftbchunks/FTBChunksClientMixin.java
@@ -7,6 +7,7 @@ import dev.ftb.mods.ftbchunks.FTBChunksCommon;
 import dev.ftb.mods.ftbchunks.client.FTBChunksClient;
 import dev.ftb.mods.ftbchunks.client.FTBChunksClientConfig;
 import dev.ftb.mods.ftblibrary.snbt.config.BooleanValue;
+import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
@@ -22,35 +23,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(FTBChunksClient.class)
 public class FTBChunksClientMixin extends FTBChunksCommon {
 
+  @Shadow
+  private static final List<Component> MINIMAP_TEXT_LIST = new ArrayList<>(3);
+
+
   @Unique
   private static final BooleanValue MINIMAP_SEASON = FTBChunksClientConfig.MINIMAP.getBoolean("season", true)
                                                                                   .comment(new String[]{
                                                                                       "Show season under minimap"});
-  @Final
-  @Shadow
-  private static List<Component> MINIMAP_TEXT_LIST;
 
-  @Inject(method = "renderHud", at = @At(value = "INVOKE", target = "Ljava/util/List;clear()V", shift = At.Shift.AFTER), remap = false)
-
+  @Inject(method = "renderHud", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;scale(FFF)V"), remap = false)
   private void renderHud(PoseStack matrixStack, float tickDelta, CallbackInfo ci) {
-    Minecraft mc = Minecraft.getInstance();
-    MutableComponent seasonCombined = CurrentSeason.getInstance(mc).getSeasonHudText();
-//    float scale = (float) ((Double) FTBChunksClientConfig.MINIMAP_SCALE.get() * 4.0 / mc.getWindow().getGuiScale());
-//    int s = (int) (64.0 * (double) scale);
-//    int x = (FTBChunksClientConfig.MINIMAP_POSITION.get()).getX(mc.getWindow().getGuiScaledWidth(), s);
-//    int y = (FTBChunksClientConfig.MINIMAP_POSITION.get()).getY(mc.getWindow().getGuiScaledHeight(), s);
-//    float iconScale = scale / 2;
-//    double s2d = (double) s / 2;
-//    int iconX = (int) (x + s2d - (double) mc.font.width(seasonCombined) / 2);
-//    int iconY = (y + s) + 3;
+    MutableComponent seasonCombined = CurrentSeason.getInstance(Minecraft.getInstance()).getSeasonHudText();
 
     Config.setEnableMod(MINIMAP_SEASON.get());
 
     if (Config.getEnableMod() && Config.getEnableMinimapIntegration()) {
       MINIMAP_TEXT_LIST.add(seasonCombined);
-//      int listPosition = MINIMAP_TEXT_LIST.indexOf(seasonCombined);
-//
-//      CurrentSeason.getInstance(mc).drawIcon(mc, matrixStack, iconScale, iconX, (iconY + (listPosition * 11)));
     }
   }
 }
